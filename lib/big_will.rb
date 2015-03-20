ce1c4c4b7fd4ee9029d1c325d45d51b7110234d3
Nadav Rosenberggi <nadav.rosenberg@gmail.com>
2015-03-20 13:12:48 +0000

    where we are now

--- lib/big_will.rb
+++ lib/big_will.rb
@@ -1,1 +1,16 @@
-#Your code goes here
+class BigWill
+  attr_reader :key_word
+  def initialize(word)
+    @key_word = word.downcase
+  end
+
+  def new_string(string)
+    key_array = @key_word.chars
+    current_letter = key_array.
+    result = ''
+    string.chars.each do |letter|
+
+
+    end
+  end
+end
--- spec/big_will_spec.rb
+++ spec/big_will_spec.rb
@@ -1,1 +1,16 @@
-#Your tests go here
+require 'big_will'
+
+
+describe 'big_will' do
+  let(:will) {BigWill.new('Will')}
+  it 'should initialize with a trget string' do
+    expect(will.key_word).to eq 'will'
+  end
+  it 'should take a string to parse through' do
+    expect(will.new_string('will test string')).to_not eq nil
+  end
+  it 'will highlight consequtive letters of the key word' do
+    expect(will.new_string('was is long life')).to eq '(w)as (i)s (l)ong (l)ife' 
+  end
+
+end
